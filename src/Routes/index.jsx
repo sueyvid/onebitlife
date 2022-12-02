8a3bf143352e2d478df6d789133843def8fe6f19
Sueyvid <sueyvidr8@gmail.com>
2022-12-02 09:14:38 -0300

    Estrutura das rotas e da HomePage

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,5 +1,8 @@
+import { useState } from "react";
 import AllPages from "./AllPages";
+import HomePage from "./HomePage";
 
-export default function Routes(){
-    return <AllPages />;
+export default function Routes() {
+    const [ showHome, setShowHome ] = useState("false");
+    return <>{showHome === "true" ? <HomePage /> : <AllPages />}</>;
 }
