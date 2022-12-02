e711381911b96b5aafcf1179dc0047b3dbc38ceb
Sueyvid <sueyvidr8@gmail.com>
2022-12-02 11:45:29 -0300

    Inserindo SelectHabit, Dropdown list e Database

--- src/Pages/HabitPage/index.jsx
+++ src/Pages/HabitPage/index.jsx
@@ -3,8 +3,11 @@ import { View, Text, StyleSheet, Image, TouchableOpacity, ScrollView, Alert } fr
 
 import { useNavigation } from "@react-navigation/native";
 
+import SelectHabit from "../../Components/HabitPage/SelectHabit";
+
 export default function HabitPage({ route }) {
     const navigation = useNavigation();
+    const [ habitInput, setHabitInput ] = useState();
     const { create, habit } = route.params;
 
     return (
@@ -26,6 +29,8 @@ export default function HabitPage({ route }) {
                         <View style={styles.inputContainer}>
                             <Text style={styles.area}>{habit?.habitArea}</Text>
                         </View>
+                        <Text style={styles.inputText}>Hábito</Text>
+                        <SelectHabit habit={habit} habitInput={setHabitInput} />
                     </View>
                 </View>
             </ScrollView>
